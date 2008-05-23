53253cd772e0b357abb94745ddc2de1a2fb315b2
Jordan Isip <jordanisip@yahoo.com>
2008-05-23 14:58:21 -0700

    Adding facebooker plugin

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,8 @@ ActionController::Routing::Routes.draw do |map|
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
+  map.root :controller => "contest"
+
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
