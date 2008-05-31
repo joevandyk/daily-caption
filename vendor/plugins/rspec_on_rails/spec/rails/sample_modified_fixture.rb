c78659893af97b93b592166020c80f9b38991487
Joe Van Dyk <joe@tanga.com>
2008-05-30 18:55:10 -0700

    Updated to rspec 1.1.4

--- vendor/plugins/rspec_on_rails/spec/rails/sample_modified_fixture.rb
+++ vendor/plugins/rspec_on_rails/spec/rails/sample_modified_fixture.rb
@@ -3,6 +3,6 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe "A sample spec", :type => :model do
   fixtures :animals
   it "should pass" do
-    animals(:pig).name.should == "Pig"
+    animals(:pig).name.should == "Piggy"
   end
 end
