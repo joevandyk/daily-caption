e886c583edbbd0e6577ac1f18361998e1ed4fded
Joe Van Dyk <joe@tanga.com>
2008-06-10 11:33:02 -0700

    You can add comments to captions

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,9 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe CommentsController do
+
+  #Delete these examples and add some real ones
+  it "should use CommentsController" do
+    controller.should be_an_instance_of(CommentsController)
+  end
+end
