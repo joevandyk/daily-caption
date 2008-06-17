3593b6662b90e4aaca080d49b7c08e5fa9ab7de9
Joe Van Dyk <joe@tanga.com>
2008-06-17 14:31:08 -0700

    Updating the profile should take place outside of Rails request loop

--- app/controllers/facebook_actions.rb
+++ app/controllers/facebook_actions.rb
@@ -2,7 +2,7 @@ module FacebookActions
   def update_fb_profile(user) 
     fb_user = user.facebook_user
     content = render_to_string(:partial=>"/users/fb_profile",:locals=>{:user=>user}) 
-    action= render_to_string(:partial=>"/users/fb_profile_action",:locals=>{:user=>user}) 
-    fb_user.set_profile_fbml(content,nil,action) 
+    action = render_to_string(:partial=>"/users/fb_profile_action",:locals=>{:user=>user}) 
+    queue_up("facebook_profile_update", content, action)
   end
 end
